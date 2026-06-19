af446d70eaaeb09667affd8a774af1173ee65dc9
MahnoorJaved34 <javedmaha394@gmail.com>
2026-06-19 21:43:29 +0500

    added class 18

---
+++ class 18/script.js
@@ -0,0 +1,16 @@
+var name = "Mahnoor Javed";
+console.log(name);
+var age = 20;
+console.log(age);
+var a = 10;
+var b = 5; 
+console.log(a + b);
+
+
+let x= 500;
+x = 1000;
+console.log(x);
+
+const y = 200;
+
+console.log(y);
