e55675c3ab7842b15d8f3a7f8ec5618b4e89c80b
MahnoorJaved34 <javedmaha394@gmail.com>
2026-06-27 19:44:34 +0500

    added folder 19

---
+++ class 19/js.js
@@ -0,0 +1,50 @@
+// -----------------OPERATORS--------------------
+let a = 10;
+let b = 5;
+console.log(a + b);
+
+let num1 = 25;
+let num2 = 26;
+console.log(num1 + num2);
+
+let num3 = 3;
+let num4 = 1;
+console.log(num3 ** num4);
+
+console.log(10 % 3);
+console.log(40+20);
+let num5 = 10;
+
+console.log(++num5);
+
+// -----------Purani values me new values ko add krna--------
+ 
+let score = 100;
+
+score += 20;
+console.log(score);
+
+// ------------Value ko kam karna-----------------
+let balance = 10000;
+
+balance -= 2500;
+console.log(balance);
+
+// -----------------value ko multiply krna -------------
+
+let price = 500;
+
+price *= 4;
+console.log(price);
+
+// -----------------value ko devide krna ------------
+
+let slices = 8;
+
+slices /= 4;
+console.log(slices);
+
+// -----------------reminder deta he----------------- 
+let number = 10;
+
+console.log(number % 3);
