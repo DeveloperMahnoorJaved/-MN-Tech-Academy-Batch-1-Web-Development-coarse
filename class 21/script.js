16944d3c21cd8fe84fdd9189356453c9ad6e03ce
MahnoorJaved34 <javedmaha394@gmail.com>
2026-07-09 22:01:40 +0500

    added folder 22

--- class 21/script.js
+++ class 21/script.js
@@ -1,2 +1,35 @@
 var city =[ "Karachi" , "lahore" , "Islamabad" , "Quetta"];
 console.log(city);
+
+// ----------Nested If---------------------
+
+
+var marks = 85;
+
+if (marks >= 50) {
+    console.log("You are Pass");
+
+    if (marks >= 80) {
+        console.log("Grade: A+");
+    }
+ }
+
+ var marks2 = 80;
+
+  if (marks2 >= 70){
+    console.log("pass hogaye ho");
+    if (marks2 >= 50){
+        console.log("grade A");
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+
---
+++ class 22/script.js
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+var city =[ "Karachi" , "Lahore" , "Islamabad"]
+
+console.log(city);
+
+// ------------Adding And Removing Elements from Array----------
+
+// -----------Method PUSH-------------
+ 
+var fruits = ["Apple", "Banana", "Mango"];
+
+fruits.push("Orange");
+
+console.log(fruits);
+
+
+
+// ------------Method POP---------------
+
+let fruits2 = ["Apple", "Banana", "Mango"];
+
+fruits2.pop();
+
+console.log(fruits2);
+
+
+
+// -----------Method UNSHIFT--------------
+
+let fruits3 = ["Apple", "Banana", "Mango"];
+
+fruits3.unshift("Orange");
+
+console.log(fruits3);
+
+// ----------------METHOD SHIFT-------------
+ 
+let fruits4 = ["Apple", "Banana", "Mango"];
+
+fruits4.shift();
+
+console.log(fruits4);
+
+// -----------------SPLICE--------------
+
+let fruits5 = ["Apple", "Banana", "Mango"];
+
+fruits5.splice(2, 2 , "Orange");
+
+console.log(fruits5);
+
+// -------------METHOD SLICE-----------------
+
+var fruits6 =["Apple" , " Banana" , "Mango"];
+
+var slicedFruits = fruits6.slice(1, 2);
+
+console.log(slicedFruits);
+
+// _____________________Method________________________
+// push()	         Last me add
+// pop()	       Last se remove
+// unshift()	    Start me add
+// shift()	      Start se remove
+// splice()	   Beech me add ya remove
+
+
+
+
+
+
+
+
+
+
